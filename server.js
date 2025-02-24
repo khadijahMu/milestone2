0423d0d50bacdbfa0edafd43bd531c46d238b805
khadijah <khadeejahmuazu@gmail.com>
2025-02-24 14:46:02 +0100

    change 6

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.use((req, res) => {
 });
 //  Start Server (Ensure Binding to "0.0.0.0" for Render)
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
